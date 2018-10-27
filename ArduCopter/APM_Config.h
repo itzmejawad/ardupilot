28a2a4077562415db3ceb5783be7ae1603ec4ceb
itzmejawad <itzmejawad@gmail.com>
2018-10-27 09:36:13 +0530

    Adding Advanced fail-safe for copter
    if 
    Fence_Action = 3
    Fence _ Enable =1
    AFS_Enable =1
    Then the copter will Disarm during fence breach , Which means Crash. The major feature required for Outback Challenge

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -50,7 +50,7 @@
 //#define CAL_ALWAYS_REBOOT                         // flight controller will reboot after compass or accelerometer calibration completes
 //#define DISALLOW_GCS_MODE_CHANGE_DURING_RC_FAILSAFE   // disable mode changes from GCS during Radio failsafes.  Avoids a race condition for vehicle like Solo in which the RC and telemetry travel along the same link
 //#define ADVANCED_FAILSAFE     ENABLED             // enabled advanced failsafe which allows running a portion of the mission in failsafe events
-
+#define ADVANCED_FAILSAFE     ENABLED               //enabled advanced failsafe which allows copter to DisArm during fence breach when Fence_Action = 3 and Fence _ Enable = 1
 // other settings
 //#define THROTTLE_IN_DEADBAND   100                // redefine size of throttle deadband in pwm (0 ~ 1000)
 
